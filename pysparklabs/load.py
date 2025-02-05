faa2f7851425a814191097ac7e4708ba4bc7a5ce
Daniel Steman <daniel-steman@live.nl>
2025-02-05 21:04:55 +0100

    get fraud dataset

--- pysparklabs/load.py
+++ pysparklabs/load.py
@@ -1,10 +1,10 @@
 import kagglehub
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("local").getOrCreate()
-path = kagglehub.dataset_download(
-    "stealthtechnologies/gdp-growth-of-european-countries"
-)
+spark = SparkSession.builder.master("local").appName("Fraud Detection").getOrCreate()  # type: ignore
+path = kagglehub.dataset_download("kartik2112/fraud-detection")
 
-df = spark.read.csv(path)
+df = spark.read.option("header", "true").csv(path)
 df.printSchema()
+
+df.show(10)
